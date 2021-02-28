cd123f69a935fb08626a95f4ad5e20e7d6128685
Michael Pohily <m.pohilyj@4slovo.ru>
2021-02-28 10:59:51 +0300

    everything works

--- TrnsportApp/ViewController.swift
+++ TrnsportApp/ViewController.swift
@@ -8,12 +8,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var switchHome: UISwitch!
+    @IBOutlet weak var labelHome: UILabel!
+    @IBOutlet weak var btnBabka: UIButton!
+    @IBOutlet weak var btnOstankino: UIButton!
+    @IBOutlet weak var btnMedvedkovo: UIButton!
+    @IBOutlet weak var btnOtradnoe: UIButton!
+    @IBOutlet weak var btnAltufan: UIButton!
+    
+    let scheduler = Scheduler()
+    
+    
+    @IBAction func actionHome(_ sender: Any) {
+        if switchHome.isOn {
+            labelHome.text = "Я дома"
+            scheduler.home = true
+        } else {
+            labelHome.text = "Я на остановке"
+            scheduler.home = false
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionBabka(_ sender: Any) {
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        if scheduler.metro() != "" {
+            secondVC.info = scheduler.metro()
+        } else {
+            secondVC.info = "Пока не едет"
+        }
+        navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+    @IBAction func actionOstankino(_ sender: Any) {
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        if scheduler.tram() != "" {
+            secondVC.info = scheduler.tram()
+        } else {
+            secondVC.info = "Пока не едет"
+        }
+        navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+    @IBAction func actionMedvedkovo(_ sender: Any) {
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        if scheduler.tram_medvedkovo() != "" {
+            secondVC.info = scheduler.tram_medvedkovo()
+        } else {
+            secondVC.info = "Пока не едет"
+        }
+        navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+    @IBAction func actionOtradnoe(_ sender: Any) {
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        if scheduler.otradnoe() != "" {
+            secondVC.info = scheduler.otradnoe()
+        } else {
+            secondVC.info = "Пока не едет"
+        }
+        navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+    @IBAction func actionAltufan(_ sender: Any) {
+        let secondVC = storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        if scheduler.altufan() != "" {
+            secondVC.info = scheduler.altufan()
+        } else {
+            secondVC.info = "Пока не едет"
+        }
+        navigationController?.pushViewController(secondVC, animated: true)
+    }
 }
-
---
+++ TrnsportApp/scheduler.swift
@@ -0,0 +1,161 @@
+//
+//  scheduler.swift
+//  Schedule
+//
+//  Created by Michael Pohily on 23.02.2021.
+//
+
+import Foundation
+
+extension URL {
+    init(_ string: StaticString) {
+        self.init(string: "\(string)")!
+    }
+}
+
+extension String {
+    func matchingStrings(regex: String) -> [[String]] {
+        guard let regex = try? NSRegularExpression(pattern: regex, options: []) else { return [] }
+        let nsString = self as NSString
+        let results  = regex.matches(in: self, options: [], range: NSMakeRange(0, nsString.length))
+        return results.map { result in
+            (0..<result.numberOfRanges).map {
+                result.range(at: $0).location != NSNotFound
+                    ? nsString.substring(with: result.range(at: $0))
+                    : ""
+            }
+        }
+    }
+}
+
+class Scheduler {
+    var home: Bool = true
+        
+    let URLS: [String: StaticString] = [
+                "bus_babka": "https://yandex.ru/maps/213/moscow/stops/stop__9641561/?ll=37.652162%2C55.871319&z=17.96",
+                "tram": "https://yandex.ru/maps/213/moscow/stops/stop__9642452/?ll=37.652162%2C55.871319&z=17.96",
+                "otradnoe": "https://yandex.ru/maps/213/moscow/stops/stop__9641574/?ll=37.652162%2C55.871319&z=17.96",
+                "avt_649": "https://yandex.ru/maps/213/moscow/stops/stop__9711575/?ll=37.652712%2C55.871607&z=18.22",
+                "tram_medvedkovo": "https://yandex.ru/maps/213/moscow/stops/stop__9642473/?ll=37.652712%2C55.871607&z=18"
+            ]
+    
+    let TIME_DELTAS = [
+        // время от квартиры до остановки
+        "bus_babka": 5,
+        "tram": 5,
+        "otradnoe": 4,
+        "avt_649": 3,
+        "tram_medvedkovo": 5
+    ]
+    
+    func on_time(data: (Int, String, String)) -> Bool {
+        var delta = 0
+        if home {
+            delta = TIME_DELTAS[data.2]!
+        }
+        if data.0 - delta >= 0 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func print_schedule(data: Array<(Int, String, String)>) -> String {
+        // время от остановки до места назначения
+        var delay: Int = 0
+        var result = ""
+        for line in data {
+            if on_time(data: line) {
+                if line.1 == "17" && line.2 == "tram_medvedkovo" {
+                    delay = 13
+                } else if line.1 == "17" {
+                    delay = 4
+                } else if line.1 == "649" {
+                    delay = 7
+                } else if line.1 == "605" || line.1 == "880" {
+                    delay = 18
+                } else {
+                    delay = 4
+                }
+                result += "\(line.1) будет через \(line.0), до метро - \(delay + line.0)\n"
+            }
+        }
+        return result
+    }
+    
+    func schedule(key: String, good: Array<String>) -> String {
+        let url = URLS[key]!
+        
+        // возвращает для конкретной остановки key массив кортежей: 0: время, 1: маршрут, 2: key
+        
+        func matches(for regex: String, in text: String) -> [String] {
+            // возвращает массив подстрок по регулярке из текста
+            do {
+                let regex = try NSRegularExpression(pattern: regex)
+                let results = regex.matches(in: text,
+                                            range: NSRange(text.startIndex..., in: text))
+                return results.map {
+                    String(text[Range($0.range(at: 1), in: text)!])
+                }
+            } catch let error {
+                print("invalid regex: \(error.localizedDescription)")
+                return []
+            }
+        }
+        
+        // маршруты
+        let content = try! NSString(contentsOf: URL(url), encoding: String.Encoding.utf8.rawValue)
+        let regex = "<ul class=\"masstransit-brief-schedule-view__vehicles\">(.*?)</ul>"
+        let roots: Array<String> = matches(for: regex, in: content as String)
+        var merged: Array<(String, String)> = []
+        for root in roots {
+            let transp_num = matches(for: " (\\d*?) в", in: root as String)
+            let time = matches(for: "<span class=\"masstransit-prognoses-view__title-text\">(\\d+?) мин</span>", in: root as String)
+            merged = zip(time, transp_num).compactMap { ($0, $1) }
+            }
+        var result: Array<(Int, String, String)> = []
+        for each in merged{
+            if good.contains(each.1) {
+                result.append((Int(each.0) ?? 0, each.1, key))
+            }
+        }
+        return self.print_schedule(data: result)
+    }
+    
+    func metro() -> String {
+        var result: String = self.avt_649()
+        result += self.tram()
+        result += self.bus_babka()
+        return result
+    }
+
+    func bus_babka() -> String {
+        let good_to_babka = ["124", "174", "238", "309", "880", "928", "С15"]
+        return self.schedule(key: "bus_babka", good: good_to_babka)
+    }
+
+    func tram() -> String {
+        let good = ["17"]
+        return self.schedule(key: "tram", good: good)
+    }
+
+    func avt_649() -> String {
+        let good = ["649"]
+        return self.schedule(key: "avt_649", good: good)
+    }
+
+    func otradnoe() -> String {
+        let good_to_otradnoe = ["605", "880"]
+        return self.schedule(key: "otradnoe", good: good_to_otradnoe)
+    }
+
+    func altufan() -> String {
+        let good = ["928"]
+        return self.schedule(key: "otradnoe", good: good)
+    }
+    
+    func tram_medvedkovo() -> String {
+        let good = ["17"]
+        return self.schedule(key: "tram_medvedkovo", good: good)
+    }
+}
